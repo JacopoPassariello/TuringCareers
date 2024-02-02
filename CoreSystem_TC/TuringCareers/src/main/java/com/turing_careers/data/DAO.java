2594cbb5063fe64321b3d8851ddb73f7402eb923
antoninoLorenzo <collab.beatbuster@gmail.com>
2024-02-02 17:40:16 +0100

    [DAO] Test Skill DAO

---
+++ CoreSystem_TC/TuringCareers/src/main/java/com/turing_careers/data/DAO.java
@@ -0,0 +1,25 @@
+package com.turing_careers.data;
+
+import jakarta.persistence.*;
+
+public abstract class DAO {
+    @PersistenceContext(type = PersistenceContextType.EXTENDED)
+    protected EntityManager em;
+    private static EntityManagerFactory emf;
+
+    public DAO() {
+        if (emf == null)
+            emf = Persistence.createEntityManagerFactory("culture_clothing");
+
+        this.em = emf.createEntityManager();
+    }
+
+    public void closeEntityManager() {
+        em.close();
+    }
+
+    protected void closeEntityManagerFactory() {
+        if (emf != null)
+            emf.close();
+    }
+}
--- CoreSystem_TC/TuringCareers/src/main/java/com/turing_careers/data/model/Employer.java
+++ CoreSystem_TC/TuringCareers/src/main/java/com/turing_careers/data/model/Employer.java
@@ -1,5 +1,15 @@
 package com.turing_careers.data.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employer {
     private int id;
     private String firstName;
@@ -7,64 +17,4 @@ public class Employer {
     private String piva;
     private String mail;
     private String password;
-
-    public Employer() {
-    }
-
-    public Employer(int id, String firstName, String lastName, String piva, String mail, String password) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.piva = piva;
-        this.mail = mail;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPiva() {
-        return piva;
-    }
-
-    public void setPiva(String piva) {
-        this.piva = piva;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
