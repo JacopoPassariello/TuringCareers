8051fac5e57e653af7dc99218614bea1fcba6f81
Antonio Pagnotta <a.pagnotta7@studenti.unisa.it>
2024-02-05 16:22:08 +0100

    Scrittura Entity
    Aggiunti variabili d'istanza, annotazioni JPA.

--- src/main/java/com/turing_careers/data/model/Location.java
+++ src/main/java/com/turing_careers/data/model/Location.java
@@ -1,17 +1,37 @@
 package com.turing_careers.data.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
+@Table(name = "Location")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "locationId", nullable = false)
     private int id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "lat", nullable = false)
     private String lat;
+
+    @Column(name = "log", nullable = false)
     private String log;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
