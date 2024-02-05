52c5f80ddcf396b31511d27b45785e172519bb09
antoninoLorenzo <collab.beatbuster@gmail.com>
2024-02-05 16:24:45 +0100

    [OfferPage] Offers made Desktop Responsive + Added Filters + Resolved Fonts

--- src/main/webapp/js/offer_page.js
+++ src/main/webapp/js/offer_page.js
@@ -7,4 +7,22 @@ $(document).ready(() => {
     toggle.click(() => {
         menu.toggleClass('display-none flex-center')
     })
+
+    /**
+     * Filters Setup
+     * */
+    const language_toggle = $("#of-filter-language-toggle")
+    const language_arrow = $("#of-filter-language-toggle-icon")
+    const language_list = $("#of-filter-language-ul")
+    language_toggle.click(() => {
+        language_list.toggleClass('display-none')
+
+        if (language_toggle.is(':checked')) {
+            language_arrow.text('expand_less')
+        }
+        else {
+            language_arrow.text('expand_more')
+        }
+    })
+
 })
