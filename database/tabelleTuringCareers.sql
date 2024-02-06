5e92be65dcbc4f3b4c148cae47c9884401c8cba3
Antonio Pagnotta <a.pagnotta7@studenti.unisa.it>
2024-02-06 23:08:00 +0100

    Modifiche a entity e aggiunta TestLocationDAO
    Modifiche a entity e aggiunta TestLocationDAO

--- database/tabelleTuringCareers.sql
+++ database/tabelleTuringCareers.sql
@@ -1,12 +1,12 @@
-drop database if exists unisa;
-create database unisa;
-use unisa;
+drop database if exists turing_careers;
+create database turing_careers;
+use turing_careers;
 
 #Location (skillId, name, type)
 CREATE TABLE Location
 (
     locationId INT NOT NULL AUTO_INCREMENT
-    ,name VARCHAR(255) NOT NULL
+    ,loc_name VARCHAR(255) NOT NULL
     ,lat FLOAT NOT NULL
     ,lon FLOAT NOT NULL
     ,PRIMARY KEY (locationId)
@@ -64,8 +64,8 @@ CREATE TABLE Offer
 CREATE TABLE Skill
 (
     skillId INT NOT NULL AUTO_INCREMENT
-    ,name VARCHAR(255) NOT NULL
-    ,type VARCHAR(10) NOT NULL
+    ,skill_name VARCHAR(255) NOT NULL
+    ,skill_type VARCHAR(255) NOT NULL
     ,PRIMARY KEY (skillId)
 );
 
