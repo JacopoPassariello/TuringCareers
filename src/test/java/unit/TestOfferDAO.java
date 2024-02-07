bba9e26f9485a1d9b8285bcc6861ea8b455ca3d6
Antonio Pagnotta <a.pagnotta7@studenti.unisa.it>
2024-02-07 19:37:30 +0100

    Aggiunti Test e modifiche offer
    Aggiunti Test e modifiche offer

--- src/test/java/unit/TestOfferDAO.java
+++ src/test/java/unit/TestOfferDAO.java
@@ -1,26 +1,39 @@
 package unit;
 
+import com.turing_careers.data.dao.EmployerDAO;
 import com.turing_careers.data.dao.LanguageDAO;
-import com.turing_careers.data.model.Language;
+import com.turing_careers.data.dao.LocationDAO;
+import com.turing_careers.data.dao.OfferDAO;
+import com.turing_careers.data.model.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestOfferDAO {
     @Test
     public void addOfferTest() throws Exception {
-        LanguageDAO languageDAO = LanguageDAO.getInstance();
-        Language language = new Language("it_IT");
+        OfferDAO offerDAO = OfferDAO.getInstance();
+        EmployerDAO employerDAO = EmployerDAO.getInstance();
+        LocationDAO locationDAO = LocationDAO.getInstance();
+        List<Language> languages = new ArrayList<>();
+        languages.add(new Language("it_IT"));
+        List<Skill> skills = new ArrayList<>();
+        Employer employer = new Employer("Antonio", "Pagnotta", "antoniopagnotta_figo@gmail.com", "123456", "TonyLoaf Inc.");
+        Location location = new Location("Milano", "3223", "2332");
+        Offer offer = new Offer("Sviluppatore Java full-stack", "Lavoro super pagato come sviluppatore Java full-stack.", "Aperta", "Remoto", employer, location, skills, languages);
 
         try {
-            languageDAO.addLanguage(language);
+            employerDAO.addEmployer(employer);
+            locationDAO.addLocation(location);
+            offerDAO.addOffer(offer);
         } catch (Exception ex) {
             Assertions.fail();
         }
 
-        List<Language> languages = languageDAO.getLanguages();
-        Assertions.assertFalse(languages.isEmpty());
-        System.out.println(languages);
+        List<Offer> offers = offerDAO.getOffers();
+        Assertions.assertFalse(offers.isEmpty());
+        System.out.println(offers);
     }
 }
