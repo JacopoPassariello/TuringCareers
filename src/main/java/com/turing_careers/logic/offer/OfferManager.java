c36bdd7403040d11933ac7c6d36c06ebed48ee65
Antonio Pagnotta <a.pagnotta7@studenti.unisa.it>
2024-02-08 18:51:47 +0100

    Modifica OfferManager
    Modifica OfferManager

--- src/main/java/com/turing_careers/logic/offer/OfferManager.java
+++ src/main/java/com/turing_careers/logic/offer/OfferManager.java
@@ -10,41 +10,47 @@ public class OfferManager {
     private static String STATE_OPEN = "OPEN";
     private static String STATE_PAUSED = "PAUSED";
     private static String STATE_CLOSED = "CLOSED";
-    private OfferDAO offerDAO;
 
-    public OfferManager() {
-        this.offerDAO = OfferDAO.getInstance();
+
+    public static void editOffer(Offer offer)  throws UpdateOfferException {
+        OfferDAO updater = OfferDAO.getInstance();
+        try {
+            updater.updateOffer(offer);
+        } catch (Exception e) {
+            throw new UpdateOfferException(e.getMessage());
+        }
     }
 
-    /**
-     *
-     */
-    public void createOffer() throws Exception {
-
+    public static void deleteOffer(Offer offer)  throws UpdateOfferException {
+        OfferDAO updater = OfferDAO.getInstance();
+        try {
+            updater.removeOffer(offer);
+        } catch (Exception e) {
+            throw new UpdateOfferException(e.getMessage());
+        }
     }
 
-    /**
-     *
-     */
-    public void modifyOffer() throws Exception {
-
+    public static void saveOffer(Offer offer)  throws UpdateOfferException {
+        OfferDAO updater = OfferDAO.getInstance();
+        try {
+            updater.addOffer(offer);
+        } catch (Exception e) {
+            throw new UpdateOfferException(e.getMessage());
+        }
     }
 
-
-    /**
-     *
-     */
     public void changeStatus(int offerId, String newState) throws Exception {
+        OfferDAO updater = OfferDAO.getInstance();
         if (newState.equals(OfferManager.STATE_CLOSED)) {
             // TODO: implement removeOfferByID
-            Offer toRemove = this.offerDAO.getOfferById(offerId);
-            this.offerDAO.removeOffer(toRemove);
+            Offer toRemove = updater.getOfferById(offerId);
+            updater.removeOffer(toRemove);
         } else {
-            Offer toUpdate = this.offerDAO.getOfferById(offerId);
+            Offer toUpdate = updater.getOfferById(offerId);
 
             toUpdate.setState(newState);
 
-            this.offerDAO.updateOffer(toUpdate);
+            updater.updateOffer(toUpdate);
         }
     }
 }
---
+++ src/main/java/com/turing_careers/logic/offer/UpdateOfferException.java
@@ -0,0 +1,10 @@
+package com.turing_careers.logic.offer;
+
+public class UpdateOfferException extends Exception {
+    public UpdateOfferException(String ex) {
+        super(ex);
+    }
+    public UpdateOfferException() {
+
+    }
+}
