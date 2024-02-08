c9494511279d7e842b562088c9d095474254369f
Antonio Pagnotta <a.pagnotta7@studenti.unisa.it>
2024-02-08 18:18:15 +0100

    Finito il testing dei DAO
    Finito il testing dei DAO [Funzionanti]

--- src/test/java/unit/TestDeveloperDAO.java
+++ src/test/java/unit/TestDeveloperDAO.java
@@ -1,21 +1,28 @@
 package unit;
 
 import com.turing_careers.data.dao.DeveloperDAO;
-import com.turing_careers.data.model.Developer;
-import com.turing_careers.data.model.Language;
+import com.turing_careers.data.dao.LocationDAO;
+import com.turing_careers.data.model.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestDeveloperDAO {
     @Test
     public void addDeveloperTest() throws Exception {
         DeveloperDAO developerDAO = DeveloperDAO.getInstance();
-        //Developer developer = new Developer("Antonio", "Pagnotta", "Java Developer", "antoniopagnottafigo@gmail.com", );
+        LocationDAO locationDAO = LocationDAO.getInstance();
+        Location location = new Location("Milano", "323", "232");
+        List<Skill> skills = new ArrayList<>();
+        List<Language> languages = new ArrayList<>();
+        List<Offer> offers = new ArrayList<>();
+        Developer developer = new Developer("Antonio", "Pagnotta", "Java Developer", "antoniopagnottafigo@gmail.com", "123456", location, skills, languages, offers);
 
         try {
-            //developerDAO.addDeveloper(developer);
+            locationDAO.addLocation(location);
+            developerDAO.addDeveloper(developer);
         } catch (Exception ex) {
             Assertions.fail();
         }
