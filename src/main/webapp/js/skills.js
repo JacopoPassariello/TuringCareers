e83e8b34659669de3df70ef8782afe297b6a2890
antoninoLorenzo <collab.beatbuster@gmail.com>
2024-02-09 11:48:36 +0100

    Backup Register Page

--- src/main/webapp/js/skills.js
+++ src/main/webapp/js/skills.js
@@ -1,11 +1,48 @@
 $(document).ready(() => {
+    /**
+     * Skill Text Input
+     * */
     const skillInput = $("#skill-input-text");
-    skillInput.on('input', function() {
+    const skillsAutocompleteSection = $("#skills-autocomplete-section");
+
+    skillInput.on('input', () => {
+        // Suggest skills while input
+        let userInput = skillInput.val();
+        if (userInput.length > 0) {
+            skillsAutocompleteSection.removeClass('display-none')
+            // TODO: query servlet to get matching skills
+            let matchingSkills = ['Java', 'Javascript'];
+            updateSuggestions(matchingSkills);
+        } else
+            skillsAutocompleteSection.addClass('display-none')
+
+        // shrink/grow text field on input
         $(this).css({
             'width': 'auto',
             'width': ($(this)[0].scrollWidth) + 'px'
         });
     }).trigger('input');
 
-    let matchingSkills = ['Java', 'Javascript'];
+    /**
+     * Shows available skills
+     * @param skills: a not empty list of skills
+     * */
+    function updateSuggestions(skills) {
+        console.log('called')
+        const skillsSuggestionsContainer = $("#skills-autocomplete-list");
+        skillsSuggestionsContainer.empty();
+
+        if (skills.length > 0) {
+            skills.forEach((item, index) => {
+                let matchingSkill = $("<p>")
+                    .text(item)
+                    .addClass('inter-regular');
+                skillsSuggestionsContainer.append(matchingSkill);
+            })
+        }
+    }
+
+    /**
+     * Location Text Input
+     * */
 })
