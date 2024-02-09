1c32d7b23182883fd0b97681d4cf554b3c1383bf
antoninoLorenzo <collab.beatbuster@gmail.com>
2024-02-09 16:47:34 +0100

    LocationClient tested

--- src/test/java/integration/LocationClientTest.java
+++ src/test/java/integration/LocationClientTest.java
@@ -1,2 +1,17 @@
-package integration;public class LocationClientTest {
+package integration;
+
+import com.turing_careers.data.model.Location;
+import com.turing_careers.logic.suggestions.LocationClient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class LocationClientTest {
+
+    @Test
+    public void testGetSuggestions() throws Exception {
+        List<Location> locations = LocationClient.getSuggestions("Rom");
+        Assertions.assertNotNull(locations.get(0));
+    }
 }
