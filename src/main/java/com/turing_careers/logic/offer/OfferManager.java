5407c6191cb2cdb3c4555b2f54fe49233714bfeb
JacopoPassariello <j.passariello@studenti.unisa.it>
2024-02-09 16:04:01 +0100

    Fix delle eccezioni lanciate dai DAO.

--- src/main/java/com/turing_careers/logic/offer/OfferManager.java
+++ src/main/java/com/turing_careers/logic/offer/OfferManager.java
@@ -1,53 +1,41 @@
 package com.turing_careers.logic.offer;
 
 import com.turing_careers.data.dao.OfferDAO;
+import com.turing_careers.data.dao.PersistenceException;
 import com.turing_careers.data.model.Developer;
 import com.turing_careers.data.model.Offer;
 import com.turing_careers.logic.validator.LanguageValidator;
 import com.turing_careers.logic.validator.SkillValidator;
 import com.turing_careers.logic.validator.ValidationException;
 
+import java.text.ParseException;
+
 /**
  *
  * */
 public class OfferManager {
 
-    public static void editOffer(Offer offer)  throws UpdateOfferException {
+    public static void editOffer(Offer offer)  throws PersistenceException {
         OfferDAO updater = OfferDAO.getInstance();
-        try {
+        updater.updateOffer(offer);
+    }
+
+    public static void deleteOffer(Offer offer)  throws PersistenceException {
+        OfferDAO updater = OfferDAO.getInstance();
+        updater.removeOffer(offer);
+
+    }
+
+    public static void createOffer(Offer offer)  throws PersistenceException {
+        OfferDAO updater = OfferDAO.getInstance();
+        updater.addOffer(offer);
+    }
+
+    public static void subscribeToOffer(Developer dev, Offer offer) throws PersistenceException {
+        OfferDAO updater = OfferDAO.getInstance();
+        if (!offer.getSubscribedDevelopers().contains((dev))) {
+            offer.getSubscribedDevelopers().add(dev);
             updater.updateOffer(offer);
-        } catch (Exception e) {
-            throw new UpdateOfferException(e.getMessage());
-        }
-    }
-
-    public static void deleteOffer(Offer offer)  throws UpdateOfferException {
-        OfferDAO updater = OfferDAO.getInstance();
-        try {
-            updater.removeOffer(offer);
-        } catch (Exception e) {
-            throw new UpdateOfferException(e.getMessage());
-        }
-    }
-
-    public static void createOffer(Offer offer)  throws UpdateOfferException {
-        OfferDAO updater = OfferDAO.getInstance();
-        try {
-            updater.addOffer(offer);
-        } catch (Exception e) {
-            throw new UpdateOfferException(e.getMessage());
-        }
-    }
-
-    public static void subscribeToOffer(Developer dev, Offer offer) throws UpdateOfferException {
-        OfferDAO updater = OfferDAO.getInstance();
-        try {
-            if (!offer.getSubscribedDevelopers().contains((dev))) {
-                offer.getSubscribedDevelopers().add(dev);
-            }
-            updater.updateOffer(offer);
-        } catch (Exception e) {
-            throw new UpdateOfferException(e.getMessage());
         }
     }
 }
