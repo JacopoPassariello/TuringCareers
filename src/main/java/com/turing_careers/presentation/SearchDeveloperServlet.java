deead221e8c9fd4d9e60a547c3f0589e32179c5c
apagnotta <a.pagnotta7@studenti.unisa.it>
2024-02-10 18:34:40 +0100

    Update SearchDeveloperServlet.java

--- src/main/java/com/turing_careers/presentation/SearchDeveloperServlet.java
+++ src/main/java/com/turing_careers/presentation/SearchDeveloperServlet.java
@@ -1,11 +1,20 @@
 package com.turing_careers.presentation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.turing_careers.data.model.Developer;
+import com.turing_careers.data.model.Employer;
+import com.turing_careers.data.model.Offer;
+import com.turing_careers.logic.search.ClientFactory;
+import com.turing_careers.logic.search.ClientType;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchDeveloperServlet extends HttpServlet {
     /**
@@ -13,7 +22,51 @@ public class SearchDeveloperServlet extends HttpServlet {
      * che l'accesso sia consentito solo ai Datori di Lavoro.
      * */
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Employer emp = null;
+
+        if(request.getSession().getAttribute("userType") == null) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+            dispatcher.forward(request, response);
+        }
+
+        String userType = (String) request.getSession().getAttribute("userType");
+
+        if(userType.equals("employer")) {
+            if(request.getSession().getAttribute("user") == null) {
+                response.setStatus(500);
+                RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+                dispatcher.forward(request, response);
+            }
+
+            emp = (Employer) request.getSession().getAttribute("user");
+
+            String query = request.getParameter("query");
+
+            if(!query.isEmpty() && !query.equals(" ")){
+
+                List<Offer> offers = new ArrayList<>();
+                        /*
+                            new ClientFactory()
+                            .setType(ClientType.DEVELOPER)
+                            .getRecommenderEngine()
+                            .search(query, emp);
+                        */
+                ObjectMapper objectMapper = new ObjectMapper();
+                String offersJSON = objectMapper.writeValueAsString(offers);
+
+                response.setContentType("application/json");
+                response.getWriter().print(offersJSON);
+                response.setStatus(200);
+            }else {
+                response.setStatus(400);
+                RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+                dispatcher.forward(request, response);
+            }
+        }else if (userType.equals("developer")) {
+            response.setStatus(400);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+            dispatcher.forward(request, response);
+        }
     }
 }
